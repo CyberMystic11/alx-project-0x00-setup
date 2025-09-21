00e76de91c8e00802ad8a249ff6bb7d0a3dae95c
Carel Seoto <carelseroto@gmail.com>
2025-09-21 19:45:24 +0200

    Render Card components on landing page

--- pages/landing.tsx
+++ pages/landing.tsx
@@ -1,9 +1,16 @@
+import Card from "@/components/Card";
+
 const Landing: React.FC = () => {
   return (
-    <div>
+    <div className="flex flex-col items-center gap-6">
       <h1 className="text-xl font-extralight">Landing Page</h1>
+      
+      {/* Render the Card component */}
+      <Card />
+      <Card />
+      <Card /> {/* Duplicate a few times to see reusability */}
     </div>
   )
 }
 
-export default Landing
+export default Landing;
